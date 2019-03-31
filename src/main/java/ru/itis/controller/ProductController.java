09a490d46d196542c69c420c5e10c4c3e12664ef
mrpokerface <imrpokerface@gmail.com>
2019-03-31 16:55:55 +0300

    review stuff added

--- src/main/java/ru/itis/controller/ProductController.java
+++ src/main/java/ru/itis/controller/ProductController.java
@@ -4,25 +4,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 import ru.itis.models.Product;
+import ru.itis.models.Review;
+import ru.itis.models.User;
 import ru.itis.services.ProductService;
+import ru.itis.services.ReviewService;
 
 @Controller
 public class ProductController {
 
     @Autowired
     private ProductService productService;
+    @Autowired
+    private ReviewService reviewService;
 
     @RequestMapping("/products/{id}")
     public ModelAndView getPage(@PathVariable("id") String stringId) {
-        Long id =Long.parseLong(stringId);
+        Long id = Long.parseLong(stringId);
         Product product = productService.getById(id);
         //name of the template is to be changed when we make it
         ModelAndView modelAndView = new ModelAndView("product_page");
         modelAndView.addObject("product", product);
+        modelAndView.addObject("reviews", reviewService.getReviewsByProductId(id));
         return modelAndView;
     }
 
-    //todo add post request mapping if needed
+    @RequestMapping("products/{id}")
+    public void saveReview(@PathVariable("id") Long id,
+                           @RequestParam("text") String text,
+                           @SessionAttribute("user") User user) {
+        Review review = Review.builder()
+                .productId(id)
+                .text(text)
+                .authorId(user.getId())
+                .build();
+        reviewService.save(review);
+        //todo change from void
+    }
 }
---
+++ src/main/java/ru/itis/repositories/ReviewRepositoryImpl.java
@@ -0,0 +1,65 @@
+package ru.itis.repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.itis.models.Review;
+
+import java.util.List;
+
+/*
+ * reviews:
+ * id
+ * productId
+ * authorId
+ * time
+ * text
+ */
+@Repository
+public class ReviewRepositoryImpl implements ReviewRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    //language=SQL
+    private static final String SQL_SELECT_BY_PRODUCT_ID_QUERY =
+            "select name as username, reviews.id, \"text\", time " +
+                    "from reviews join users on reviews.author_id = users.id " +
+                    "where product_id = ?";
+    //language=SQL
+    private static final String SQL_INSERT_QUERY = "insert into reveiws (author_id, text, product_id, time) values (?, ?, ?, now())";
+
+    private RowMapper<Review> rowMapperWithUsername = (resultSet, i) ->
+            Review.builder()
+                    .id(resultSet.getLong("id"))
+                    .text(resultSet.getString("text"))
+                    .time(resultSet.getTimestamp("time"))
+                    .username(resultSet.getString("username"))
+                    .build();
+
+    @Override
+    public List<Review> getByPostId(Long id) {
+        return jdbcTemplate.query(SQL_SELECT_BY_PRODUCT_ID_QUERY, rowMapperWithUsername, id);
+    }
+
+    @Override
+    public List<Review> findAll() {
+        return null;
+    }
+
+    @Override
+    public Review find(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Review model) {
+        jdbcTemplate.update(SQL_INSERT_QUERY, model.getAuthorId(), model.getText(), model.getProductId(), model.getTime());
+    }
+
+    @Override
+    public void update(Review model) {
+
+    }
+}
